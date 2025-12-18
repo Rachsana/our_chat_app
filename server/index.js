e8daebc9f560783e867364a878116172d54b8e31
Rachsana <rrachnapatel137@gmail.com>
2025-12-19 04:09:50 +0530

    Update CORS middleware configuration

--- server/index.js
+++ server/index.js
@@ -9,7 +9,10 @@ const chatRoutes = require('./routes/chat');
 const app = express();
 
 // Middleware
-app.use(cors());
+app.use(cors({
+  origin: "*",
+  credentials: true
+}));
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
